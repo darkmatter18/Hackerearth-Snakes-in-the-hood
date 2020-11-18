35a8ab8a122b730ea2ef3b45827114feded7d074
Arkadip <in2arkadipb13@gmail.com>
2020-11-18 19:57:24 +0530

    feat: cnn_model.py completed

--- model/cnn_model.py
+++ model/cnn_model.py
@@ -3,6 +3,7 @@ import torch
 import itertools
 from torch import nn, optim
 from .networks import CnnEncoder, LinearDecoder
+from utils import f1_loss
 
 
 class CnnModel:
@@ -33,6 +34,10 @@ class CnnModel:
         self.label_original = None
         self.label_pred = None
 
+        self.training_loss = 0
+        self.test_loss = 0
+        self.f1_scores = 0
+
     def feed_input(self, x: dict):
         """Unpack input data from the dataloader and perform necessary pre-processing steps.
 
@@ -52,7 +57,11 @@ class CnnModel:
         self.optimizer.zero_grad()
         # print(self.label_pred)
         # print(self.label_original)
-        self.criterion(self.label_pred, self.label_original)
+        loss = self.criterion(self.label_pred, self.label_original)
+        loss.backward()
+        train_loss = loss.item()
+        self.training_loss += train_loss
+        # print("Train Loss", train_loss)
         self.optimizer.step()
 
     def forward(self):
@@ -69,3 +78,52 @@ class CnnModel:
     def eval(self):
         self.cnn_encoder.eval()
         self.linear_decoder.eval()
+
+    def get_last_avg_train_loss(self):
+        loss = self.training_loss / self.opt.print_freq
+        self.training_loss = 0
+        return loss
+
+    def _single_test(self, data):
+        self.feed_input(data)
+        self.forward()
+        test_loss = self.criterion(self.label_pred, self.label_original).item()
+        # print("Test Loss", test_loss)
+        self.test_loss += test_loss
+        self.f1_scores += f1_loss(self.label_original, self.label_pred).item()
+
+    def run_test_on_training(self, testloader) -> tuple:
+        with torch.no_grad():
+            no = 0
+            for i, data in enumerate(testloader):
+                self._single_test(data)
+                no = i + 1
+        # print("Length of testloader", no)
+        test_loss = self.test_loss / no
+        f1_score = self.f1_scores / no
+        self.test_loss = 0
+        self.f1_scores = 0
+
+        return test_loss, f1_score
+
+    def save_networks(self, epoch: str) -> None:
+        """Save models
+        :param epoch: Current Epoch (prefix for the name)
+        """
+        self.save_network(self.cnn_encoder, 'cnn_encoder', epoch)
+        self.save_network(self.linear_decoder, 'linear_decoder', epoch)
+
+    def save_network(self, net, net_name, epoch):
+        save_filename = f'{epoch}_net_{net_name}.pt'
+        save_path = os.path.join(self.save_dir, save_filename)
+
+        if len(self.gpu_ids) > 0 and torch.cuda.is_available():
+            torch.save(net.module.cpu().state_dict(), save_path)
+            net.cuda(self.gpu_ids[0])
+        else:
+            torch.save(net.cpu().state_dict(), save_path)
+
+    def save_optimizer(self, epoch):
+        save_path = os.path.join(self.save_dir, f"{epoch}_optimizer.pt")
+
+        torch.save(self.optimizer.state_dict(), save_path)
