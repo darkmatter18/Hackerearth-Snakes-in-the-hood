f44aeb89f13e7cbb05a7cbc0c6e9cc27a766d79c
Arkadip <in2arkadipb13@gmail.com>
2020-11-18 19:58:09 +0530

    feat: Train loop done

--- sih/train.py
+++ sih/train.py
@@ -2,6 +2,7 @@ import time
 from data import create_dataset
 from model.cnn_model import CnnModel
 from options.TrainOptions import TrainOptions
+from utils.TestStats import TrainStats
 
 
 def main():
@@ -9,6 +10,7 @@ def main():
     trainloader, testloader = create_dataset(opt)
 
     model = CnnModel(opt)
+    stats = TrainStats(opt)
 
     total_iters = 0                # the total number of training iterations
     for epoch in range(opt.epoch_count, opt.n_epochs + 1):
@@ -29,6 +31,27 @@ def main():
             if total_iters % opt.print_freq == 0:
                 t_data = iter_start_time - epoch_start_time
                 t_comp = (time.time() - iter_start_time) / opt.batch_size
+                train_loss = model.get_last_avg_train_loss()
+                test_loss, f1_score = model.run_test_on_training(testloader)
+                stats.print_current_losses(epoch, epoch_iter, train_loss, test_loss, f1_score, t_comp, t_data)
+
+            if total_iters % opt.save_latest_freq == 0:
+                print('saving the latest model (epoch %d, total_iters %d)' % (epoch, total_iters))
+                save_suffix = 'iter_%d' % total_iters if opt.save_by_iter else 'latest'
+                model.save_networks(save_suffix)
+                model.save_optimizer(save_suffix)
+
+        if epoch % opt.save_epoch_freq == 0:
+            print('saving the model at the end of epoch %d, iters %d' % (epoch, total_iters))
+            model.save_networks('latest')
+            model.save_optimizer('latest')
+            model.save_networks(str(epoch))
+            model.save_optimizer(str(epoch))
+
+        print('End of epoch %d / %d \t Time Taken: %d sec' %
+              (epoch, opt.n_epochs + opt.n_epochs_decay, time.time() - epoch_start_time))
+
+    print("End of Training")
 
 
 if __name__ == '__main__':
