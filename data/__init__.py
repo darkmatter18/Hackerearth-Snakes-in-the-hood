3eff99ee46cf5b6bc3ad121f7de1f9e779930258
Arkadip <in2arkadipb13@gmail.com>
2020-11-19 23:04:01 +0530

    feat: Test Dataset created

--- data/__init__.py
+++ data/__init__.py
@@ -2,7 +2,7 @@ import os
 import math
 import pickle
 import pandas as pd
-from .datasets import SnakeDataset, SnakeTestDataset
+from .datasets import SnakeDataset
 from .dataloaders import SnakeDataLoader
 
 
@@ -27,9 +27,10 @@ def create_dataset(opt):
     print(f"No of train data is {len(train_data)}",
           f"No of test data is {len(test_data)}", sep="\n")
 
-    train_dataset = SnakeDataset(opt.dataroot, opt.phase, train_data, opt.preprocess, True, opt.load_size,
+    train_dataset = SnakeDataset(opt.dataroot, opt.phase, train_data, opt.preprocess, True, True, opt.load_size,
                                  opt.crop_size)
-    test_dataset = SnakeDataset(opt.dataroot, opt.phase, test_data, opt.preprocess, False, opt.load_size, opt.crop_size)
+    test_dataset = SnakeDataset(opt.dataroot, opt.phase, test_data, opt.preprocess, False, True, opt.load_size,
+                                opt.crop_size)
 
     trainloader = SnakeDataLoader(train_dataset, opt.batch_size, opt.num_threads, not opt.no_train_shuffle)
     testloader = SnakeDataLoader(test_dataset, opt.batch_size, opt.num_threads, shuffle=False)
@@ -40,7 +41,8 @@ def create_dataset(opt):
 def create_test_dataset(opt):
     data = pd.read_csv(f'{opt.dataroot}/{opt.phase}.csv')['image_id'].values
 
-    test_dataset = SnakeTestDataset(opt.dataroot, opt.phase, data, opt.preprocess, opt.load_size, opt.crop_size)
+    test_dataset = SnakeDataset(opt.dataroot, opt.phase, data, opt.preprocess, False, False, opt.load_size,
+                                opt.crop_size)
     testloader = SnakeDataLoader(test_dataset, opt.batch_size, opt.num_threads, shuffle=False)
 
     return testloader
--- data/datasets.py
+++ data/datasets.py
@@ -9,22 +9,33 @@ from torchvision.transforms import Resize, CenterCrop, RandomRotation, RandomHor
 
 class SnakeDataset(Dataset):
     def __init__(self, dataroot: str, phase: str, data: np.array, preprocess: str = 'resize,crop,rotate,flip',
-                 apply_augmentation: bool = False, load_size: int = 156, crop_size: int = 128):
+                 apply_augmentation: bool = False, test_mode: bool = False, load_size: int = 156, crop_size: int = 128):
         self.data = data
         self.preprocess = preprocess
         self.load_size = load_size
         self.crop_size = crop_size
+        self.test_mode = test_mode
 
         self.transforms = self.get_transform(self.preprocess, augmentation=apply_augmentation)
         self.image_dir = os.path.join(dataroot, phase)
 
     def __getitem__(self, index):
-        d = self.data[index, :]
-        file = os.path.join(self.image_dir, f'{d[0]}.jpg')
+        if not self.test_mode:
+            d = self.data[index, :]
+            f_name = d[0]
+        else:
+            d = self.data[index]
+            f_name = d
+        file = os.path.join(self.image_dir, f'{f_name}.jpg')
         img = Image.open(file).convert('RGB')
         img_t = self.transforms(img)
-        label = torch.from_numpy(np.array(d[-1], dtype=np.int64))
-        return {'image': img_t, 'label': label}
+
+        if not self.test_mode:
+            label = torch.from_numpy(np.array(d[-1], dtype=np.int64))
+        else:
+            label = None
+
+        return {'image': img_t, 'label': label, 'image_id': f_name}
 
     def __len__(self):
         return len(self.data)
@@ -40,44 +51,6 @@ class SnakeDataset(Dataset):
 
         if 'flip' in preprocess and augmentation:
             transform_list.append(RandomHorizontalFlip())
-
-        if 'crop' in preprocess:
-            transform_list.append(CenterCrop(self.crop_size))
-
-        if convert:
-            transform_list.append(ToTensor())
-            transform_list.append(Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]))
-
-        return Compose(transform_list)
-
-
-class SnakeTestDataset(Dataset):
-    def __init__(self, dataroot: str, phase: str, data: np.array, preprocess: str = 'resize,crop,rotate,flip',
-                 load_size: int = 156, crop_size: int = 128):
-        self.data = data
-        self.preprocess = preprocess
-        self.load_size = load_size
-        self.crop_size = crop_size
-
-        self.transforms = self.get_transform(self.preprocess)
-        self.image_dir = os.path.join(dataroot, phase)
-
-    def __getitem__(self, index):
-        d = self.data[index]
-        file = os.path.join(self.image_dir, f'{d}.jpg')
-        img = Image.open(file).convert('RGB')
-        img_t = self.transforms(img)
-        label = torch.from_numpy(np.array(d[-1], dtype=np.int64))
-        return {'image': img_t, 'label': label, 'image_id': d}
-
-    def __len__(self):
-        return len(self.data)
-
-    def get_transform(self, preprocess: str, convert=True):
-        transform_list = []
-
-        if 'resize' in preprocess:
-            transform_list.append(Resize([self.load_size, self.load_size]))
 
         if 'crop' in preprocess:
             transform_list.append(CenterCrop(self.crop_size))
