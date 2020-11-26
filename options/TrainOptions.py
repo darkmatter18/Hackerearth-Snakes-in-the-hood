be1d7d35648a37138c2aad6a0e8c9748c7a6bcd3
Arkadip <in2arkadipb13@gmail.com>
2020-11-26 11:19:49 +0530

    feat: Changed save_latest_freq to 400 from 600, added output log, added log entries

--- options/TrainOptions.py
+++ options/TrainOptions.py
@@ -22,7 +22,7 @@ class TrainOptions(BaseOptions):
         parser.add_argument('--epoch_count', type=int, default=1,
                             help='the starting epoch count, '
                                  'we save the model by <epoch_count>, <epoch_count>+<save_latest_freq>,')
-        parser.add_argument('--save_latest_freq', type=int, default=600, help='frequency of saving the latest results')
+        parser.add_argument('--save_latest_freq', type=int, default=400, help='frequency of saving the latest results')
         parser.add_argument('--save_epoch_freq', type=int, default=4,
                             help='frequency of saving checkpoints at the end of epochs')
         parser.add_argument('--save_by_iter', action='store_true', help='whether saves model by iteration')
