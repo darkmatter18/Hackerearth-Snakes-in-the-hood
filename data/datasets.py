cb95721067304e3fa32d8617c91b56dac2bc4356
Arkadip <in2arkadipb13@gmail.com>
2020-12-16 06:47:55 +0000

    refactor: train augmentation Resize value changed to crop_size from load_size

--- data/datasets.py
+++ data/datasets.py
@@ -50,7 +50,7 @@ class SnakeDataset(Dataset):
             ])
         else:
             return Compose([
-                Resize(self.load_size),
+                Resize(self.crop_size),
                 CenterCrop(self.crop_size),
                 ToTensor(),
                 Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
